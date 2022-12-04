a1d050efcc8aa89faf89d9812d7936ab25051f93
edtimer <b4timer@protonmail.com>
2022-12-04 15:28:35 +0000

    added github pages configuration to docusaurus config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,17 +8,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'UTM development environment setup tutorial site',
   tagline: 'By: Abdulla Adam',
-  url: 'https://google.com',
-  baseUrl: '/',
+  url: 'https://github.com',
+  baseUrl: '/utm-code-tutorials/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'UTM', // Usually your GitHub org/user name.
-  projectName: 'UTM dev', // Usually your repo name.
-
+  organizationName: 'edtimer', // Usually your GitHub org/user name.
+  projectName: 'utm-code-tutorials', // Usually your repo name.
+  deploymentBranch:"gh-pages",
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
